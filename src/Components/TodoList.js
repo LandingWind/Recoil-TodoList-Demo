7f27f889f07ecf0521ea35cc82f4bda8a6b349e4
wkk5194 <kysoowjk@163.com>
2020-05-25 02:53:07 +0800

    add todolist async sources from server

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import { TodoItem, cstrTodoItem } from './TodoItem';
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { useRecoilState, useRecoilValue, useRecoilValueLoadable } from 'recoil';
 import {
     listDataState,
     filteredListState,
-    filterState
+    filterState,
+    queryListDataState
 } from '../store';
 import md5 from 'blueimp-md5'
 
@@ -17,6 +18,11 @@ function TodoList() {
     const [filter, setFilter] = useRecoilState(filterState);
     const filteredListData = useRecoilValue(filteredListState);
 
+    const queryListData = useRecoilValueLoadable(queryListDataState);
+    if(queryListData.state === "hasValue") {
+        setListData(queryListData.getValue())
+    }
+    
     return (
         <div className="list">
             <ul>
@@ -26,6 +32,14 @@ function TodoList() {
                     )
                 })}
             </ul>
+            {/* <h4>Todos From Server:</h4>
+            <ul>
+                {queryListData.state === "hasValue" ? queryListData.getValue().map((item, index) => {
+                    return (
+                        <li key={index + md5(item.label)}><TodoItem itemdata={item} /></li>
+                    )
+                }):null}
+            </ul> */}
             <div>
                 <span>
                     <input
